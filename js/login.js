043f0598d8bed1662fd2ca963bc61ca114bb88ff
Eric DeLabar <edelabar@gmail.com>
2011-04-15 21:25:52 -0400

    third test passed

--- js/login.js
+++ js/login.js
@@ -4,6 +4,8 @@
 		return this.each(function(){	
 			$(this).find(':text,:password').focus(function(){
 				$(this).prev().hide();
+			}).blur(function(){
+				$(this).prev().show();
 			});
 
 			$(this).addClass('enhanced');
