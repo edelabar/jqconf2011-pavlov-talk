0f18489ae98fcadbeba497f00523b57921a999c0
Eric DeLabar <edelabar@gmail.com>
2011-04-15 21:24:59 -0400

    second test passed

--- js/login.js
+++ js/login.js
@@ -1,7 +1,11 @@
 (function($){
 
 	$.fn.loginForm = function(){	
-		return this.each(function(){
+		return this.each(function(){	
+			$(this).find(':text,:password').focus(function(){
+				$(this).prev().hide();
+			});
+
 			$(this).addClass('enhanced');
 		});
 	};
