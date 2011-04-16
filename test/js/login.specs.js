9e5a7788d764b300d5748e5f6c04d23ceb6b03d7
Eric DeLabar <edelabar@gmail.com>
2011-04-15 21:24:37 -0400

    the rest of the second test written

--- test/js/login.specs.js
+++ test/js/login.specs.js
@@ -13,12 +13,19 @@ QUnit.specify("My Awesome WebApp",function(){
 				form = $('#login').loginForm();
 			});
 
-			it('should hide a field label when the field has focus.');
+			it('should hide a field label when the field has focus.',function(){
+				form.find(':text,:password').each(function(){
+					var label = $(this).prev();
+					$(this).focus();
+					assert(label.css("display")).isEqualTo('none');
+				});
+			});
+
 			it('should show a field label when the field is empty when the cursor leaves.');
 			it('should keep a field label hidden when the field is not empty when the cursor leaves.');
 			it('should validate that the email field is valid when the cursor leaves.');
 			it('should submit the form when the user clicks the "Join" button.');
-			it('should submit to the action URL of the form with an AJAX POST.');	
+			it('should submit to the action URL of the form with an AJAX POST.');
 		});
 	});
 });
